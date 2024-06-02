d5f60de9853bcf669c22c1b870e2b8b7c0ad0381
arpit2707 <arpitgaurav.goldi723@gmail.com>
2024-06-02 22:57:17 +0530

    Email Verification Done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { firebaseEmailVerifyURL } from "./store/constants";
+import axios from "axios";
 
 const Welcome = () => {
+  const verify = async () => {
+    const vrification = await axios.post(firebaseEmailVerifyURL, {
+      requestType: "VERIFY_EMAIL",
+      idToken: localStorage.getItem("token"),
+    });
+    console.log(vrification);
+  };
   return (
     <div className="d-flex justify-content-center flex-column align-items-center">
       <header>Welcome to home screen</header>
-      <p>
-        Your Profile is incomplete <Link to="/update">Complete Now</Link>
-      </p>
+      <Link to="/update">Profile</Link>
+      <button className="btn btn-primary" onClick={verify}>
+        Verify Email
+      </button>
     </div>
   );
 };
--- src/components/store/constants.js
+++ src/components/store/constants.js
@@ -3,3 +3,4 @@ export const firebaseLoginURL = `https://identitytoolkit.googleapis.com/v1/accou
 export const firebaseSignUpURL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 export const firebaseUpdateURL = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_KEY}`;
 export const firebaseProfileURL = `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${API_KEY}`;
+export const firebaseEmailVerifyURL = `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${API_KEY}`;
